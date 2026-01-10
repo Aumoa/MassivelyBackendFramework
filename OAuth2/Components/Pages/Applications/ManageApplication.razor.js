5182d3c3f3ea65d4a08a98d023a6ef2d1b6b608d
liberty <wkdghcjf1234@gmail.com>
2026-01-10 20:24:06 +0900

    클라이언트 조회, 생성 관련 오류 수정

--- OAuth2/Components/Pages/Applications/ManageApplication.razor.js
+++ OAuth2/Components/Pages/Applications/ManageApplication.razor.js
@@ -1,7 +1,1 @@
-﻿function getFragmentParams() {
-    const hash = window.location.hash.substring(1); // '#' 제거
-    const params = new URLSearchParams(hash);
-    const clientId = params.get('client_id');
-    const secret = params.get('secret');
-    return { clientId, secret };
-}
+﻿
