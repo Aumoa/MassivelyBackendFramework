16bc3a32072c4a1bd044c462f2cbae061eab4ed1
liberty <wkdghcjf1234@gmail.com>
2026-03-15 00:56:46 +0900

    컴파일 오류 수정

---
+++ OpenAI.Core/DapperUtility/MessageRoleHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using Dapper;
+
+namespace OpenAI.DapperUtility;
+
+internal class MessageRoleHandler : SqlMapper.TypeHandler<MessageRole>
+{
+    public override MessageRole Parse(object value)
+    {
+        return (MessageRole)Convert.ToSByte(value);
+    }
+
+    public override void SetValue(IDbDataParameter parameter, MessageRole value)
+    {
+        parameter.Value = (sbyte)value;
+    }
+}
